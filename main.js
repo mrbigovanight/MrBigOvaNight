b30cb3b3b2c663d721773d759245251d8189ed7a
ranger097 <ranger097@proton.me>
2026-06-16 02:56:56 -0400

    delete old images, added drop menu style selections

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 var menu = document.getElementById("menu");
 menu.classList.add("none");
-
 function menuToggle() {
 var menu = document.getElementById("menu");
 menu.classList.toggle("none");
 
-if (menu.classList.contains("none")) {
-checkbox.checked = false;
-} else {
-checkbox.checked = true;
 }
+
+const programLink1 = document.getElementById("programLink1");
+programLink1.classList.add("none");
+function programToggle() {
+  programLink1.classList.toggle("none");
 }
+
+const videoLink1 = document.getElementById("videoLink1");
+const videoLink2 = document.getElementById("videoLink2");
+const videoLink3 = document.getElementById("videoLink3");
+videoLink1.classList.add("none");
+videoLink2.classList.add("none");
+videoLink3.classList.add("none");
+function videoToggle() {
+  videoLink1.classList.toggle("none");
+  videoLink2.classList.toggle("none");
+  videoLink3.classList.toggle("none");
+}
+
+const articleLink1 = document.getElementById("articleLink1");
+const articleLink2 = document.getElementById("articleLink2");
+const articleLink3 = document.getElementById("articleLink3");
+articleLink1.classList.add("none");
+articleLink2.classList.add("none");
+articleLink3.classList.add("none");
+function articleToggle() {
+  articleLink1.classList.toggle("none");
+  articleLink2.classList.toggle("none");
+  articleLink3.classList.toggle("none");
+}
+
+
