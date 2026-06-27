21b23c488de22758ba2aed8e7f1fbdcb50640432
ranger097 <ranger097@proton.me>
2026-06-27 12:09:50 -0400

    added supabase init and restructured the files into directories

---
+++ Scripts/login.js
@@ -0,0 +1,22 @@
+import { supabase } from './supabase.js'
+
+const form = document.getElementById('loginForm')
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+    return
+  }
+
+  window.location.href = "/dashboard.html"
+})
---
+++ Scripts/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://bcegumgewrotirizmtda.supabase.co', 'sb_publishable_t6XEnrxIbOJ5CgSa-l4NfA_DQrcoKMN')
+
+
