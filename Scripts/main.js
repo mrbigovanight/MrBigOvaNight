725f1d590728fe0110e778aa3fc5a1ab9bf31108
ranger097 <ranger097@proton.me>
2026-07-19 21:10:30 -0400

    adjusted layout and colors of na for products page

--- Scripts/main.js
+++ Scripts/main.js
@@ -33,33 +33,40 @@ link.innerHTML = '&#9747; Close';
 }
 
 const programLink1 = document.getElementById("programLink1");
-//if (programLink1) {
-//programLink1.classList.add("none");
-//}
+const programLink2 = document.getElementById("programLink2");
+const programLink3 = document.getElementById("programLink3");
+const programLink4 = document.getElementById("programLink4");
 function programToggle() {
   if (programLink1) {
   programLink1.classList.toggle("none");
+  programLink2.classList.toggle("none");
+  programLink3.classList.toggle("none");
+  programLink4.classList.toggle("none");
   }
 }
 
 const videoLink1 = document.getElementById("videoLink1");
 const videoLink2 = document.getElementById("videoLink2");
 const videoLink3 = document.getElementById("videoLink3");
-//videoLink1.classList.add("none");
-//videoLink2.classList.add("none");
-//videoLink3.classList.add("none");
+const imageLink1 = document.getElementById("imageLink1");
+const imageLink2 = document.getElementById("imageLink2");
+const imageLink3 = document.getElementById("imageLink3");
+
 function videoToggle() {
   videoLink1.classList.toggle("none");
   videoLink2.classList.toggle("none");
   videoLink3.classList.toggle("none");
 }
 
+function imageToggle() {
+  imageLink1.classList.toggle("none");
+  imageLink2.classList.toggle("none");
+  imageLink3.classList.toggle("none");
+}
+
 const articleLink1 = document.getElementById("articleLink1");
 const articleLink2 = document.getElementById("articleLink2");
 const articleLink3 = document.getElementById("articleLink3");
-//articleLink1.classList.add("none");
-//articleLink2.classList.add("none");
-//articleLink3.classList.add("none");
 function articleToggle() {
   articleLink1.classList.toggle("none");
   articleLink2.classList.toggle("none");
@@ -73,13 +80,7 @@ const socialLink4 = document.getElementById("socialLink4");
 const socialLink5 = document.getElementById("socialLink5");
 const socialLink6 = document.getElementById("socialLink6");
 const socialLink7 = document.getElementById("socialLink7");
-//socialLink1.classList.add("none");
-//socialLink2.classList.add("none");
-//socialLink3.classList.add("none");
-//socialLink4.classList.add("none");
-//socialLink5.classList.add("none");
-//socialLink6.classList.add("none");
-//socialLink7.classList.add("none");
+
 function socialToggle() {
 socialLink1.classList.toggle("none");
 socialLink2.classList.toggle("none");
@@ -92,8 +93,7 @@ socialLink7.classList.toggle("none");
 
 const aboutLink1 = document.getElementById("aboutLink1");
 const aboutLink2 = document.getElementById("aboutLink2");
-//aboutLink1.classList.add("none");
-//aboutLink2.classList.add("none");
+
 function aboutToggle() {
   aboutLink1.classList.toggle("none");
   aboutLink2.classList.toggle("none");
--- Scripts/programs.js
+++ Scripts/programs.js
@@ -25,11 +25,13 @@ console.log("LENGTH:", data?.length);
 
   data.forEach(program => {
   programDiv.innerHTML += `
-   <div class="card">
+   <a class="card" href="program.html?slug=${program.slug}">
       <img class="cardBanner" src="${program.banner}" alt="${program.title}"/>
+      <div class="descriptionContainer">
       <h2 class="cardTitle">${program.title}</h2>
       <p class="cardDescription">${program.description}</p>
-    </div>`;
+      </div>
+    </a>`;
   });
 
 }
@@ -40,19 +42,24 @@ var menu = document.getElementById("menu");
 if (menu) {
 menu.classList.add("none");
 }
+
 var link = document.getElementById("header-menu-span");
 if (link) {
 link.innerHTML = '&#9776; Menu';
 }
+
 function menuToggle() {
 var menu = document.getElementById("menu");
 var headerMenu = document.getElementById("header-link-container");
+
 if (headerMenu) {
 headerMenu.classList.toggle("new-header-link-container");
+programDiv.classList.toggle("newProgram");
 }
 var header = document.querySelector("header");
 if (menu) {
 menu.classList.toggle("none");
+
 }
 if (header) {
 header.classList.toggle("newHeader");
@@ -61,6 +68,7 @@ const text = link.textContent.trim();
 if (text.includes('Close')) {
 if (link) {
 link.innerHTML = '&#9776; Menu';
+
 }
 } else {
 if (link) {
@@ -70,34 +78,43 @@ link.innerHTML = '&#9747; Close';
 
 }
 
+
+
 const programLink1 = document.getElementById("programLink1");
-if (programLink1) {
-programLink1.classList.add("none");
-}
+const programLink2 = document.getElementById("programLink2");
+const programLink3 = document.getElementById("programLink3");
+const programLink4 = document.getElementById("programLink4");
 function programToggle() {
   if (programLink1) {
   programLink1.classList.toggle("none");
+  programLink2.classList.toggle("none");
+  programLink3.classList.toggle("none");
+  programLink4.classList.toggle("none");
   }
 }
 
 const videoLink1 = document.getElementById("videoLink1");
 const videoLink2 = document.getElementById("videoLink2");
 const videoLink3 = document.getElementById("videoLink3");
-videoLink1.classList.add("none");
-videoLink2.classList.add("none");
-videoLink3.classList.add("none");
+const imageLink1 = document.getElementById("imageLink1");
+const imageLink2 = document.getElementById("imageLink2");
+const imageLink3 = document.getElementById("imageLink3");
+
 function videoToggle() {
   videoLink1.classList.toggle("none");
   videoLink2.classList.toggle("none");
   videoLink3.classList.toggle("none");
 }
 
+function imageToggle() {
+  imageLink1.classList.toggle("none");
+  imageLink2.classList.toggle("none");
+  imageLink3.classList.toggle("none");
+}
+
 const articleLink1 = document.getElementById("articleLink1");
 const articleLink2 = document.getElementById("articleLink2");
 const articleLink3 = document.getElementById("articleLink3");
-articleLink1.classList.add("none");
-articleLink2.classList.add("none");
-articleLink3.classList.add("none");
 function articleToggle() {
   articleLink1.classList.toggle("none");
   articleLink2.classList.toggle("none");
@@ -111,13 +128,7 @@ const socialLink4 = document.getElementById("socialLink4");
 const socialLink5 = document.getElementById("socialLink5");
 const socialLink6 = document.getElementById("socialLink6");
 const socialLink7 = document.getElementById("socialLink7");
-socialLink1.classList.add("none");
-socialLink2.classList.add("none");
-socialLink3.classList.add("none");
-socialLink4.classList.add("none");
-socialLink5.classList.add("none");
-socialLink6.classList.add("none");
-socialLink7.classList.add("none");
+
 function socialToggle() {
 socialLink1.classList.toggle("none");
 socialLink2.classList.toggle("none");
@@ -130,8 +141,7 @@ socialLink7.classList.toggle("none");
 
 const aboutLink1 = document.getElementById("aboutLink1");
 const aboutLink2 = document.getElementById("aboutLink2");
-aboutLink1.classList.add("none");
-aboutLink2.classList.add("none");
+
 function aboutToggle() {
   aboutLink1.classList.toggle("none");
   aboutLink2.classList.toggle("none");
